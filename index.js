b6496362cd776cb1c41deaa21e618e708679ec08
Mykhailo Koretskyi <radio.mkor@gmail.com>
2016-07-31 16:21:49 -0300

    Playing with exports

--- index.js
+++
@@ -1,4 +0,0 @@
-export TextInput from './src/jsx/textInput';
-export EmailInput from './src/jsx/emailInput';
-export PasswordInput from './src/jsx/passwordInput';
-export RadioButton from './src/jsx/radioButton';
---
+++ src/index.js
@@ -0,0 +1,6 @@
+'use strict';
+
+export TextInput from './jsx/textInput';
+export EmailInput from './jsx/emailInput';
+export PasswordInput from './jsx/passwordInput';
+export RadioButton from './jsx/radioButton';
